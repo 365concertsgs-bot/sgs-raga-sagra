4c8d777d1b841c4daf82e049103e85b045f805a8
SGS User <365concertsgs@gmail.com>
2026-05-25 11:32:15 +0530

    Fix mobile viewport fit and responsive body layout

--- src/App.jsx
+++ src/App.jsx
@@ -673,15 +673,19 @@ export default function App({ leftLogoUrl = "https://i.imgur.com/lPDE0zB.jpeg", 
           font-family: 'Philosopher', serif;
           -webkit-font-smoothing: antialiased;
           -moz-osx-font-smoothing: grayscale;
-          overflow: hidden;
+          overflow-x: hidden;
+          overflow-y: auto;
+          min-height: 100%;
+          position: relative;
+          background: #000;
         }
 
         html, body {
           width: 100%;
-          height: 100%;
+          min-height: 100%;
           margin: 0;
           padding: 0;
-          position: fixed;
+          position: relative;
         }
         
         a[href]:hover {
@@ -1301,8 +1305,8 @@ export default function App({ leftLogoUrl = "https://i.imgur.com/lPDE0zB.jpeg", 
 /* 🎨 STYLES */
 const styles = {
   container: {
-    width: "100vw",
-    height: "100vh",
+    width: "100%",
+    minHeight: "100vh",
     position: "relative",
     overflow: "hidden",
     background:
